faf46f6a9259f6246111b1dc7e4c1c57226efb25
ousmanekedougou <yabaye075@gmail.com>
2021-11-22 23:00:23 +0000

    commit du fichier mail

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => ['MAIL_MAILER' => 'smtp'],
 
     /*
     |--------------------------------------------------------------------------
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' =>  'smtp.gmail.com',
+            'port' =>  587,
+            'encryption' => 'tls',
+            'username' => 'ousmanelaravel@gmail.com',
+            'password' => 'baye13811',
             'timeout' => null,
             'auth_mode' => null,
         ],
