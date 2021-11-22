3c914c735b7d02b456ecd0955a9a559cbb726877
ousmanekedougou <yabaye075@gmail.com>
2021-11-22 22:12:35 +0000

    le fichier mail

--- config/mail.php
+++ config/mail.php
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ADMIN_SUPPORT_EMAIL'),
+        'name' => env('MAIL_FROM_NAME', 'APP_NAME'),
     ],
 
     /*
