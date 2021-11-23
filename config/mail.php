c1afe96f3ad6ba7e60e5a0bf893919d3b94dfc8c
ousmanekedougou <yabaye075@gmail.com>
2021-11-23 00:51:00 +0000

    commit du fichier mail

--- config/mail.php
+++ config/mail.php
@@ -97,8 +97,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ousmanelaravel@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'AEERK'),
     ],
 
     /*
