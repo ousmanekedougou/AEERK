429fd0922e80f2ee85841e59589ef4216d1f7b72
ousmanekedougou <yabaye075@gmail.com>
2022-02-19 00:07:29 +0000

    commit du setting

--- resources/views/user/systeme/index.blade.php
+++ resources/views/user/systeme/index.blade.php
@@ -81,10 +81,16 @@
 
 	<section class="mt-5">
 		<div class="container" style="">
-			<h3 class="text-heading text-center text-uppercase">Comment s'inscrire pour les codifications</h3>
-			<p class="sample-text text-justify">
-				Every avid independent filmmaker has <b>Bold</b> about making that <i>Italic</i> interest documentary, or short film to show off their creative prowess. Many have great ideas and want to “wow” the<sup>Superscript</sup> scene, or video renters with their big project.  But once you have the<sub>Subscript</sub> “in the can” (no easy feat), how do you move from a <del>Strike</del> through of master DVDs with the <u>“Underline”</u> marked hand-written title inside a secondhand CD case, to a pile of cardboard boxes full of shiny new, retail-ready DVDs, with UPC barcodes and polywrap sitting on your doorstep?  You need to create eye-popping artwork and have your project replicated. Using a reputable full service DVD Replication company like PacificDisc, Inc. to partner with is certainly a helpful option to ensure a professional end result, but to help with your DVD replication project, here are 4 easy steps to follow for good DVD replication results: 
-			</p>
+				<div class="row">
+					<div class="col-lg-2"></div>
+					<div class="col-lg-8">
+						<h3 class="text-heading text-center text-uppercase">Comment s'inscrire pour les codifications</h3>
+						<p class="sample-text text-justify">
+							“Recently, the US Federal government banned online casinos from operating in America by making it illegal to transfer money to them through any US bank or payment system. As a result of this law, most of the popular online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino players found themselves being chased by the Federal government. But, after a fortnight, the online casino industry came up with a solution and new online casinos started taking root. These began to operate under a different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major part of this was enlisting electronic banking systems that would accept this new clarification and start doing business with me. Listed in this article are the electronic banking” 
+						</p>
+					</div>
+					<div class="col-lg-2"></div>
+				</div>
 		</div>
 	</section>	
 	
@@ -92,35 +98,41 @@
 		<div class="container" style="">
 			<div class="section-top-border">
 				<div class="row">
-					<div class="col-lg-12">
-						<blockquote class="generic-blockquote text-justify">
-							<h3 class="mb-30 text-uppercase text-left">Pour les nouveaux</h3>
+					<div class="col-lg-2"></div>
+					<div class="col-lg-8">
+						<h3 class="mb-30 text-uppercase text-center">Pour les nouveaux</h3>
+						<p class="text-justify">
 							“Recently, the US Federal government banned online casinos from operating in America by making it illegal to transfer money to them through any US bank or payment system. As a result of this law, most of the popular online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino players found themselves being chased by the Federal government. But, after a fortnight, the online casino industry came up with a solution and new online casinos started taking root. These began to operate under a different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major part of this was enlisting electronic banking systems that would accept this new clarification and start doing business with me. Listed in this article are the electronic banking” 
-						</blockquote>
+						</p>
 					</div>
+					<div class="col-lg-2"></div>
 				</div>
 			</div>
 
 
 			<div class="section-top-border">
 				<div class="row">
-				<div class="col-lg-12">
-						<blockquote class="generic-blockquote text-justify">
-							<h3 class="mb-30 text-uppercase text-left">Pour les anciens</h3>
+					<div class="col-lg-2"></div>
+					<div class="col-lg-8">
+						<h3 class="mb-30 text-uppercase text-center">Pour les anciens</h3>
+						<p class="text-justify">
 							“Recently, the US Federal government banned online casinos from operating in America by making it illegal to transfer money to them through any US bank or payment system. As a result of this law, most of the popular online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino players found themselves being chased by the Federal government. But, after a fortnight, the online casino industry came up with a solution and new online casinos started taking root. These began to operate under a different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major part of this was enlisting electronic banking systems that would accept this new clarification and start doing business with me. Listed in this article are the electronic banking” 
-						</blockquote>
+						</p>
 					</div>
+					<div class="col-lg-2"></div>
 				</div>
 			</div>
 
 			<div class="section-top-border">
 				<div class="row">
-					<div class="col-lg-12">
-						<blockquote class="generic-blockquote text-justify">
-							<h3 class="mb-30 text-uppercase text-left">Consernant les recasements</h3>
-							“Recently, the US Federal government banned online casinos from operating in America by making it illegal to transfer money to them through any US bank or payment system. As a result of this law, most of the popular online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino players found themselves being chased by the Federal government. But, after a fortnight, the online casino industry came up with a solution and new online casinos started taking root. These began to operate under a different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major part of this was enlisting electronic banking systems that would accept this new clarification and start doing business with me. Listed in this article are the electronic banking” 
-						</blockquote>
+					<div class="col-lg-2"></div>
+					<div class="col-lg-8">
+							<h3 class="mb-30 text-uppercase text-center">Consernant les recasements</h3>
+							<p class="text-justify">
+								“Recently, the US Federal government banned online casinos from operating in America by making it illegal to transfer money to them through any US bank or payment system. As a result of this law, most of the popular online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino players found themselves being chased by the Federal government. But, after a fortnight, the online casino industry came up with a solution and new online casinos started taking root. These began to operate under a different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major part of this was enlisting electronic banking systems that would accept this new clarification and start doing business with me. Listed in this article are the electronic banking” 
+							</p>
 					</div>
+					<div class="col-lg-2"></div>
 				</div>
 			</div>
 		</div>
