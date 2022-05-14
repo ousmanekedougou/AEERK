a373bf9906a3dcdb2a86a58d127a55e4d0910d25
ousmanekedougou <yabaye075@gmail.com>
2022-05-14 12:26:32 +0000

    commit du 14/15/2022

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1211,6 +1211,7 @@ class DatabaseSeeder extends Seeder
                    'sendmail' => 'aeerk.sn@gmail.com',
                    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
                    'lien' => 2,
+                   'lien_front' => 2,
                    'sendPhone' => 221781956168,
                    'text_dechifre' => 'password'
                 ]);
